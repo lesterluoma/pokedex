ce4dfe992c47ad8837afeeedd236ddf03745a29b
Lester Luoma <lesterdluoma@gmail.com>
2024-09-15 22:18:42 -0400

    updated PokeAPI call

--- src/services/pokeapi.js
+++ src/services/pokeapi.js
@@ -1,17 +1,15 @@
 export const fetchPokemonData = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-    console.log(json);
-    return json;
+    const json = await response.json(); // Wait for the promise to resolve
+    return json; // Return the JSON data
   } catch (error) {
-    console.error(error.message);
+    console.error("Error fetching Pokémon data:", error);
     return null;
   }
 };
